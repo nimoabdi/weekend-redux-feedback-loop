0cf9434acc1f268c716608774c4d6cdf6d0a42b4
Nimo Abdi <nimoabdi318@gmail.com>
2022-06-17 19:04:39 -0500

    working on comments and creating dispatch for it

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,28 @@
+import { useDispatch } from 'react-redux';
+import  { useState } from 'react';
+
+function Comments () {
+    const dispatch = useDispatch();
+    const [comments, setComments] = useState('') 
+    const getComments = (event) => {
+        event.preventDefault();
+        console.log('this is the comment rating', comments)
+        dispatch({
+            type: 'COMMENT_NUMBER',
+            payload: comments
+        })
+    }
+    return (
+        <section>
+            <header>
+                <h1>Any comments you want to leave?</h1>
+                <p>Comments</p>
+
+
+
+
+            </header>
+        </section>
+    ) 
+}
+export default Comments;
