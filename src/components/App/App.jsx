bf0c384466d2c5fb670c378961fcd14d69089c2a
Nimo Abdi <nimoabdi318@gmail.com>
2022-06-18 23:57:19 -0500

    making routes and history

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,6 +6,7 @@ import Comments from '../Comments/Comments';
 import Feeling from '../Feeling/Feeling';
 import Support from '../Support/Support';
 import Understanding from '../Understanding/Understanding';
+import Review from '../Review/Review';
 
 function App() {
 
@@ -17,22 +18,24 @@ function App() {
         <h4>Don't forget it!</h4>
       </header>
     </div>
-    <Route>
-      
-    </Route>
-    <Route>
-      <Comments />
-    </Route>
-    <Route>
+    <Route exact path='/' >
+      <p>Enter feedback</p>
       <Feeling />
     </Route>
-    <Route>
-      <Support />
-    </Route>
-    <Route>
+    <Route exact path='/Understanding'>
       <Understanding />
     </Route>
-
+    <Route exact path='Support'>
+      <Support />
+    </Route>
+    <Route exact path='Comments'>
+      <Comments />
+    </Route>
+    <Route exact path='Review'>
+      <Review />
+    </Route>
+    
+    
   </Router>  
   );
 }
--- src/components/SubmitThankYou/SubmitThankYou.jsx
+++ src/components/SubmitThankYou/SubmitThankYou.jsx
@@ -4,3 +4,4 @@ function SubmitThankYou() {
         </>
     )
 }
+export default SubmitThankYou;
